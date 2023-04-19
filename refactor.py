e950150dc03659ae94df9a756486b0fcef9d1b2f
Sourcery AI <>
2023-04-19 14:02:11 +0000

    'Refactored by Sourcery'

--- refactor.py
+++ refactor.py
@@ -1,25 +1,16 @@
 # assign-if-exp
 condition = True
-if condition:
-    x = 1
-else:
-    x = 2
-
-
-# Augmented assign
-count = 2
+x = 1 if condition else 2
 other_value = 2
-count = count + other_value
-
-
+count = 2 + other_value
 # Chain compares
 b = 2
-if 1 < b and b < 3:
+if 1 < b < 3:
     print("b is between 1 and 3")
 
 
 # convert-any-to-in
 hats = ["basker", "cap", "bowler"]
-if any(hat == "bowler" for hat in hats):
+if "bowler" in hats:
     print("I have a bowler hat!")
 
